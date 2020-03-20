90e8d79d1b6a0306fa71e67ed52acb1810f0dc91
Alexander Manea <alexander.manea@gmail.com>
2020-03-20 17:15:46 +0200

    fix tests

--- src/test/scala/spoolphiz/twitterstats/service/StatsServiceSpec.scala
+++ src/test/scala/spoolphiz/twitterstats/service/StatsServiceSpec.scala
@@ -1,5 +1,8 @@
 package spoolphiz.twitterstats.service
 
+import java.time.Instant
+
+import com.danielasfregola.twitter4s.entities.Tweet
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -7,7 +10,8 @@ class StatsServiceSpec extends AnyWordSpec with Matchers {
   "StatsService" should {
     "create map of emoji to number of occurrences in a string" in {
       val str = "An \uD83D\uDE00awesome \uD83D\uDE00string with a \uD83D\uDE00few \uD83D\uDE09emojis!"
-      val result = StatsService.extractEmojis(str)
+      val tweet = Tweet(text = str, created_at = Instant.now, id = 1L, id_str = "1", source = "")
+      val result = StatsService.extractEmojis(tweet)
       result.size shouldEqual 2
       result("\uD83D\uDE00") shouldEqual 3
     }
